e54dd08431b48d201c97fad82ee4b5cc68cb7151
Tarun Sharma <tarunsharma08.com@gmail.com>
2025-05-21 19:18:14 +0530

    abc

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
 import type { NextConfig } from "next";
+import { createMDX } from 'fumadocs-mdx/next';
+
+const withMDX = createMDX();
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 's3.amazonaws.com',
-  //       port: '',
-  //       pathname: '/my-bucket/**',
-  //       search: '',
-  //     },
-  //   ],
-  // },
+  
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
